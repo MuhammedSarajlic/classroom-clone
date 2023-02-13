01f300a1eb5bc013bedd84592fb85b176a589e25
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-13 13:56:52 +0100

    feat: User registration controller

--- server/Classroom-API/Classroom-API/Controllers/RegisterController.cs
+++ server/Classroom-API/Classroom-API/Controllers/RegisterController.cs
@@ -7,5 +7,19 @@ namespace Classroom_API.Controllers
     [ApiController]
     public class RegisterController : ControllerBase
     {
+
+        private readonly DataContext _context;
+        public RegisterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Users.ToListAsync());
+        }
     }
 }
