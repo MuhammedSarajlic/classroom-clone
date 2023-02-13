423d8f9edcce95b79e2230238529554ff00a65b6
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-14 00:17:32 +0100

    feat: Register user from frontend

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,47 @@
+import axios from 'axios';
+import { useState } from 'react';
 import GoogleLogo from '../assets/google-icon.svg';
 
 const Register = () => {
+  const [error, setError] = useState({
+    type: '',
+    message: '',
+  });
+  const [userData, setUserData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const addUser = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (userData.password === userData.confirmPassword) {
+      axios
+        .post('https://localhost:7155/api/register/add', userData)
+        .then(() => {
+          setUserData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+          });
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setError({
+        type: 'password',
+        message: "Password doesn't match",
+      });
+    }
+  };
+
+  const updateData = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <section className=''>
@@ -14,9 +55,47 @@ const Register = () => {
           <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
               <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-                Create and account
+                Create account
               </h1>
-              <form className='space-y-4 md:space-y-6' action='#'>
+              <form className='space-y-4 md:space-y-6' onSubmit={addUser}>
+                <div className='flex space-x-5'>
+                  <div>
+                    <label
+                      htmlFor='firstName'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      First Name
+                    </label>
+                    <input
+                      type='text'
+                      name='firstName'
+                      id='firstName'
+                      value={userData.firstName}
+                      onChange={updateData}
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      placeholder='John'
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor='lastName'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      type='text'
+                      name='lastName'
+                      id='lastName'
+                      value={userData.lastName}
+                      onChange={updateData}
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      placeholder='Doe'
+                      required
+                    />
+                  </div>
+                </div>
                 <div>
                   <label
                     htmlFor='email'
@@ -28,6 +107,8 @@ const Register = () => {
                     type='email'
                     name='email'
                     id='email'
+                    value={userData.email}
+                    onChange={updateData}
                     className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     placeholder='name@company.com'
                     required
@@ -44,6 +125,8 @@ const Register = () => {
                     type='password'
                     name='password'
                     id='password'
+                    value={userData.password}
+                    onChange={updateData}
                     placeholder='••••••••'
                     className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     required
@@ -51,15 +134,17 @@ const Register = () => {
                 </div>
                 <div>
                   <label
-                    htmlFor='confirm-password'
+                    htmlFor='confirmPassword'
                     className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                   >
                     Confirm password
                   </label>
                   <input
-                    type='confirm-password'
-                    name='confirm-password'
-                    id='confirm-password'
+                    type='password'
+                    name='confirmPassword'
+                    id='confirmPassword'
+                    value={userData.confirmPassword}
+                    onChange={updateData}
                     placeholder='••••••••'
                     className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                     required
