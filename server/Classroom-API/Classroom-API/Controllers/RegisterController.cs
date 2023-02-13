e5bf6d2c71ea186d482e6522463febec9ec7789b
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-14 00:44:32 +0100

    feat: Add user data verification
    -Check if the user with that email already exists
    -Check if the password is less than 8 characters

--- server/Classroom-API/Classroom-API/Controllers/RegisterController.cs
+++ server/Classroom-API/Classroom-API/Controllers/RegisterController.cs
@@ -18,10 +18,13 @@ namespace Classroom_API.Controllers
 
         [HttpPost]
         [Route("register/add")]
-        public async Task<ActionResult<List<User>>> AddUser(User user)
+        public async Task<ActionResult<User>> AddUser(User user)
         {
-            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-            user.Password = hashedPassword;
+            if (_context.Users.Any(u => u.Email == user.Email))
+                return BadRequest("User already exists");
+            if (user.Password.Length < 8)
+                return BadRequest("Password is too short");
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return Ok(user);
