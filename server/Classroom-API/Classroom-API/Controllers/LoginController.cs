09d2702b60e1a2ddbf688ead79f1b2e6d2759eb0
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-13 23:00:13 +0100

    feat: Add login post method and UserCreds model

---
+++ server/Classroom-API/Classroom-API/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using BCrypt.Net;
+using Classroom_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classroom_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CheckUser(LoginCreds creds)
+        {
+            var userDb = _context.Users.Where(u => u.Email == creds.Email).FirstOrDefault();
+            if (userDb == null) return BadRequest("User not found");
+            if (!BCrypt.Net.BCrypt.Verify(creds.Password, userDb.Password))
+            {
+                return BadRequest("Password incorrect");
+            }
+
+            return Ok("You are signed in");
+        }
+    }
+}
---
+++ server/Classroom-API/Classroom-API/Models/LoginCreds.cs
@@ -0,0 +1,9 @@
+﻿namespace Classroom_API.Models
+{
+    public class LoginCreds
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
+    }
+}
