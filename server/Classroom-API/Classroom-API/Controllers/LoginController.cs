0b8ace59be55a1928c1c94d7cb7703b1c351db5d
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-14 23:57:11 +0100

    feat: Create token function
    -Create a token when the user successfully login

--- server/Classroom-API/Classroom-API/Controllers/LoginController.cs
+++ server/Classroom-API/Classroom-API/Controllers/LoginController.cs
@@ -2,21 +2,28 @@
 using Classroom_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Classroom_API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class LoginController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _config;
 
-        public LoginController(DataContext context)
+        public LoginController(DataContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpPost]
+        [Route("login")]
         public async Task<ActionResult<string>> CheckUser(LoginCreds creds)
         {
             var userDb = _context.Users.Where(u => u.Email == creds.Email).FirstOrDefault();
@@ -26,7 +33,33 @@ namespace Classroom_API.Controllers
                 return BadRequest("Password incorrect");
             }
 
-            return Ok("You are signed in");
+            string token = CreateToken(userDb);
+
+            return Ok(token);
         }
+        [NonAction]
+        public string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+            
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        } 
     }
 }
