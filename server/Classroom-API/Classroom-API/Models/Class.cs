dc072a0b6a3b148575d37d254dc576d4b88c3cb0
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-16 00:46:30 +0100

    feat: Add class model and update user model

---
+++ server/Classroom-API/Classroom-API/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace Classroom_API.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Section { get; set; } = string.Empty;
+        public int TeacherId { get; set; } // Foreign key
+
+        // Navigation properties
+        //public User? Teacher { get; set; }
+        public ICollection<User>? Students { get; set; }
+    }
+}
