c86a670a9dad7c0a0b35de657579b773a00a400e
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-16 23:27:21 +0100

    Create many to many relationship

--- server/Classroom-API/Classroom-API/Data/DataContext.cs
+++ server/Classroom-API/Classroom-API/Data/DataContext.cs
@@ -8,5 +8,22 @@ namespace Classroom_API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         
         public DbSet<User> Users { get; set; }
+        public DbSet<Class> Classes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Class>()
+                .HasMany(c => c.Students)
+                .WithMany(u => u.Classes)
+                .UsingEntity<Dictionary<string, object>>(
+                    "ClassStudent",
+                    x => x.HasOne<User>().WithMany().HasForeignKey("UserId"),
+                    x => x.HasOne<Class>().WithMany().HasForeignKey("ClassId")
+                );
+        }
+
     }
+
 }
