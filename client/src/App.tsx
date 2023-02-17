c6c62f9614db39d549defd9ee1fdc844df8261e8
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-02-17 23:59:57 +0100

    feat: Decode token and return and store user data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,28 +4,33 @@ import { getCookie } from 'typescript-cookie';
 import { Context } from './helper/Context';
 import Navbar from './components/Navbar';
 import { useState } from 'react';
+import { AxiosResponse } from 'axios';
 
 function App() {
   const token = getCookie('jwt_token');
   const [addClassModal, setAddClassModal] = useState(false);
+  const [user, setUser] = useState<AxiosResponse | null | void>(null);
 
   return (
-    <Context.Provider value={{ token }}>
-      <Navbar setAddClassModal={setAddClassModal} />
-      <Routes>
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route
-          path='/'
-          element={
-            <Home
-              addClassModal={addClassModal}
-              setAddClassModal={setAddClassModal}
-            />
-          }
-        />
-      </Routes>
-    </Context.Provider>
+    <>
+      <Context.Provider value={{ token }}>
+        <Navbar setAddClassModal={setAddClassModal} />
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route
+            path='/'
+            element={
+              <Home
+                addClassModal={addClassModal}
+                setAddClassModal={setAddClassModal}
+                setUser={setUser}
+              />
+            }
+          />
+        </Routes>
+      </Context.Provider>
+    </>
   );
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,37 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getCookie } from 'typescript-cookie';
 import ClassFormModal from '../components/ClassFormModal';
 import { Context } from '../helper/Context';
+import jwt_decode, { JwtPayload } from 'jwt-decode';
 
 interface Props {
   addClassModal: boolean;
   setAddClassModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setUser: React.Dispatch<
+    React.SetStateAction<void | AxiosResponse<any, any> | null>
+  >;
 }
 
-export const Home = ({ addClassModal, setAddClassModal }: Props) => {
+interface tokenValue {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export const Home = ({ addClassModal, setAddClassModal, setUser }: Props) => {
   const navigate = useNavigate();
-  const token = useContext(Context);
+  const token = JSON.stringify(useContext(Context));
 
   useEffect(() => {
+    const decoded = jwt_decode<tokenValue>(token || '');
     axios
-      .get('https://localhost:7155/api/Home', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+      .post('https://localhost:7155/api/Home', decoded)
+      .then((res) => {
+        setUser(res);
+        console.log(res);
       })
-      .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }, []);
 
